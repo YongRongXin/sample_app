0c975f0c3be66a8e5407c5040326a3a3c7be80a7
YongRongXin <abxin19970508@gmail.com>
2020-08-27 16:10:23 +0800

    Chapter 8: Basic Login

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,5 @@ require('@rails/ujs').start()
 require('turbolinks').start()
 require('@rails/activestorage').start()
 require('channels')
+require('jquery')
+import 'bootstrap'
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get "static_pages/help"
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :users
 end
